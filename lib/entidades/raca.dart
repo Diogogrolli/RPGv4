b9a84de8b7919ab933d0d21aabe97b7c8a42c235
Lucas Henrique de Abreu <lucas.abreu@betrybe.com>
2025-09-09 20:44:11 -0300

    IMplementado raças

---
+++ lib/entidades/raca.dart
@@ -0,0 +1,11 @@
+abstract class Raca {
+  final int bonusVida;
+  final int bonusEscudo;
+  final int bonusAtaque;
+
+  Raca({
+    required this.bonusVida,
+    required this.bonusEscudo,
+    required this.bonusAtaque,
+  });
+}
